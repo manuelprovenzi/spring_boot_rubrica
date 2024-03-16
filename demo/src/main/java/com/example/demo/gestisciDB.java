1a0fc9eff81f059af6f861a1f3c71381a0653713
Manuel <manuel14.provenzi@gmail.com>
2024-03-16 11:54:50 +0100

    gestisci db, spiegazione

---
+++ demo/src/main/java/com/example/demo/gestisciDB.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class gestisciDB {
+    Connection conn;
+
+    gestisciDB() throws SQLException{
+        //root è l'utente, l'altro è la password
+        conn = DriverManager.getConnection("jdbc:mysql://localhost/test","root","");//puo connettersi a qualsiasi db e file (cambierà la stringa) -> data source
+
+        Statement statement = conn.createStatement();//quando dobbiamo lanciare una query
+        ResultSet rs = statement.executeQuery("SELECT id,nome as alias FROM utenti");
+        //rs.next(); //true-> esiste un elemento successivo
+
+        while(rs.next()){
+            int id = rs.getInt(0); //0 è la colonna
+            String user = rs.getString("alias"); //"alias" è la colonna 1 -> 2 modi
+        }
+
+        rs.close();
+        statement.close();
+
+        PreparedStatement stat2 = conn.prepareStatement("select * from utenti where username=? and password=md5(?)");
+        stat2.setString(1, "mario");
+        stat2.setString(2, "porta");
+        ResultSet rs2 = stat2.executeQuery("SELECT id,nome as alias FROM utenti");
+
+        while(rs2.next()){
+            int id = rs2.getInt(0); //0 è la colonna
+            String user = rs.getString("alias"); //"alias" è la colonna 1 -> 2 modi
+        }
+    }
+
+}
